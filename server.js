9399566696abab483d9471aec5de1c5af1ab3ad2
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2018-08-18 11:18:51 -0400

    updated

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ app.use(bodyParser.json());
 
 //any input data ==>
 
+var reservation = [];
+var waitlist = [];
+
 //routes
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html")); 
 });
 
+app.post("api/new", function(req,res){
+    var newReso = req.body;
+    newReso.routeName = newReso.name.replace(/\s+/g, "").toLowerCase();
+    reservation.push(newReso);
+    res.json(newReso);
+})
 
 //start server to listen 
 
