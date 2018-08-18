7fc65a63000dded1c50167280ddc5fa1dc53785a
System Administrator <root@Nenads-MacBook-Pro.local>
2018-08-18 11:18:29 -0400

    Tables created with data to populate existing reservations

--- server.js
+++ server.js
@@ -1,20 +1,67 @@
-var express = require("express"); 
-var bodyParser = require("body-parser"); 
-var path = require("path"); 
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-var app = express(); 
-var PORT = 3000; 
+var app = express();
+var PORT = 3000;
 
-//express app to handle data parsing 
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(bodyParser.json()); 
+//express app to handle data parsing
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//tables with data to populate some existing reservations
+
+var tables = [
+	{
+		name: 'George',
+		phonenumber: 9042658617,
+		email: 'mymail@mail.com',
+		uniqueId:454
+	},
+	{
+		name: 'Kyle',
+		phonenumber: 555555555,
+		email: 'kyle@mail.com',
+		uniqueId: 500
+	},
+	{
+		name: 'Stone',
+		phonenumber: 555555555,
+		email: 'stone@mail.com',
+		uniqueId: 500
+	},
+	{
+		name: 'Jason',
+		phonenumber: 555555555,
+		email: 'jason@mail.com',
+		uniqueId: 500
+	},
+	{
+		name: 'Robert',
+		phonenumber: 555555555,
+		email: 'robert@mail.com',
+		uniqueId: 500
+	},
+	{
+		name: 'Charley',
+		phonenumber: 555555555,
+		email: 'chrarley@mail.com',
+		uniqueId: 500
+	},
+	{
+		name: 'Tina',
+		phonenumber: 555555555,
+		email: 'tina@mail.com',
+		uniqueId: 500
+	}
+];
 
 //any input data ==>
 
 //routes
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "index.html")); 
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/reserve", function(req, res) {
@@ -25,8 +72,8 @@ app.get("/tables", function(req, res){
     res.sendFile(path.join(__dirname, "tables.html"))
 })
 
-//start server to listen 
+//start server to listen
 
 app.listen(PORT, function() {
-    console.log("App listening on Port: " + PORT); 
+    console.log("App listening on Port: " + PORT);
 });
