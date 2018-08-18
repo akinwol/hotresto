9d6e6c3f3882fe649e9c65b4bc7af55420dbad1e
Marshall Johnston <marshalljohnston77@gmail.com>
2018-08-18 11:05:57 -0400

    created routes

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html")); 
 });
 
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"))
+})
+
+app.get("/tables", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"))
+})
 
 //start server to listen 
 
